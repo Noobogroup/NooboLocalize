0c7b47b5b1a4d6b7ec870fe1e82a5b3b210921d1
Noobogroup <82022035+Noobogroup@users.noreply.github.com>
2021-04-26 12:10:06 +0430

    Add files via upload

---
+++ Runtime/NooboLocalizeSettings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NooboPackage.NooboLocalize.Runtime
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "Settings", menuName = "NooboPackage/Localization/Settings")]
+    public class NooboLocalizeSettings : ScriptableObject, ISerializationCallbackReceiver
+    {
+        private static NooboLocalizeSettings _;
+        
+        [SerializeField] public string selectedLocale;
+        [SerializeField] public List<LocalizedTable> tables;
+
+        private List<Locale> _locales;
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Initialize()
+        {
+            var settings = Resources.Load<NooboLocalizeSettings>("NooboLocalize/settings");
+            if (settings == null)
+            {
+                throw new Exception(
+                    "No NooboLocalize Settings exist! try to create one by clicking on Window/NooboLocalize in toolbar.");
+            }
+            
+            settings.Set();
+        }
+        
+        
+
+        private void Set()
+        {
+            _ = this;
+            _locales = Resources.LoadAll<Locale>("NooboLocalize/Locales").ToList();
+            tables = Resources.LoadAll<LocalizedTable>("NooboLocalize/Tables").ToList();
+        }
+    
+        public static Locale CurrentLocale()
+        {
+            return _._locales.Find(locale => locale.name == _.selectedLocale);
+        }
+        
+        // TODO: locales should be only created via settings and with predefined names
+        // TODO: Show a selectable and editable list of locales in settings
+
+        
+        
+        public void OnBeforeSerialize()
+        {
+            tables?.Clear();
+            tables = Resources.LoadAll<LocalizedTable>("NooboLocalize/Tables").ToList();
+        }
+
+        public void OnAfterDeserialize()
+        {
+        }
+    }
+}
